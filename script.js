078f2a2dd94d445ea70a48ac435a7aecb2477ebf
Sophie Alexandre <55605315+sophiealexandre@users.noreply.github.com>
2020-07-27 15:48:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const player = document.getElementById('player');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const captureButton = document.getElementById('capture');
+
+const constraints = {
+  video: true,
+};
+
+captureButton.addEventListener('click', () => {
+  // Draw the video frame to the canvas.
+  context.drawImage(player, 0, 0, canvas.width, canvas.height);
+});
+
+// Attach the video stream to the video element and autoplay.
+navigator.mediaDevices.getUserMedia(constraints)
+  .then((stream) => {
+    player.srcObject = stream;
+  });
+
+
+function myFunction() {
+   var element = document.getElementById("canvas");
+   var filter = document.getElementById('filter').value;
+   console.log(filter)
+   element.classList.add(filter);
+}
